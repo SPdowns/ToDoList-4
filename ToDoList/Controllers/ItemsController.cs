47afb3527bae5ce5e3eeb25a4cab3248076f0022
Joseph Pearce <joseph.pearce05@gmail.com>
2020-07-29 13:55:42 -0700

    final ToDoList Project
    Co-Authored-By: Sean Downs<spdowns@protonmail.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -3,6 +3,8 @@ using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace ToDoList.Controllers
 {
@@ -16,12 +18,13 @@ namespace ToDoList.Controllers
     }
     public ActionResult Index()
     {
-      List<Item> model = _db.Items.ToList();
+      List<Item> model = _db.Items.Include(items => items.Category).ToList();
       return View(model);
     }
 
     public ActionResult Create()
     {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View();
     }
 
@@ -32,6 +35,7 @@ namespace ToDoList.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+    
 
     public ActionResult Details(int id)
     {
@@ -43,6 +47,7 @@ namespace ToDoList.Controllers
     public ActionResult Edit(int id)
     {
     var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+    ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
     return View(thisItem);
     }
 
